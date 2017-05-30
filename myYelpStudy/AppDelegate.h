ef4c84e6cfbabc98d5d81fc797bc04694fafbbdd
yue <zhyue@umich.edu>
2017-05-30 10:31:41 -0700

    Initial Commit

---
+++ myYelpStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myYelpStudy
+//
+//  Created by CAO YUE on 5/30/17.
+//  Copyright © 2017 ZHANG YUE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myYelpStudy/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  myYelpStudy
+//
+//  Created by CAO YUE on 5/30/17.
+//  Copyright © 2017 ZHANG YUE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
